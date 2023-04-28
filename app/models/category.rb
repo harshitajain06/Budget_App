51f5b9b998e4211a59a28b8afe813fde7cb0a194
Harshita Jain <harshitaforever06@gmail.com>
2023-04-28 13:28:14 +0530

    fix linters

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,6 @@ class Category < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_and_belongs_to_many :transacs
 
-
   validates :name, presence: true
   validates :name, length: { minimum: 3, maximum: 50 }
 end
