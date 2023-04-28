d899b147223d95e430e026782c433d832b4c9101
Harshita Jain <harshitaforever06@gmail.com>
2023-04-28 18:40:26 +0530

    rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-   
+
   has_many :categories, foreign_key: 'author_id'
   has_many :transacs, foreign_key: 'author_id'
   validates :name, presence: true
